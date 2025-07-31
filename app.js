2ad0ebb9d0b7927156f1061e3bcc77a1aad224f5
Hanzala Khan <46111633+hanzalakhan010@users.noreply.github.com>
2025-07-31 05:58:28 +0000

    removed unused

--- app.js
+++ app.js
@@ -12,7 +12,6 @@ const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 app.use(express.static(path.join(__dirname, 'assets')))
 
-// let loggedIn = false
 
 
 
